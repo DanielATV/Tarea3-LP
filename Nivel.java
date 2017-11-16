693f6eedd58dad666d416753e0be50d8d9758c15
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-16 18:31:58 -0300

    Update Nivel.java

--- Nivel.java
+++ Nivel.java
@@ -1,1 +1,22 @@
+package juego;
 
+public class Nivel{
+	
+	String nombreNivel;
+	String nombreJugador;
+	int vidaJugador;
+	int defensaJugador;
+	String ataqueEspecial1;
+	String ataqueEspecial2;
+	String nombreJefe;
+	int vidaJefe;
+	int defensaJefe;
+	String ataqueSpecialjefe;
+	int vidaAliado;
+	String objetoAliado;
+	String objetoNivel;
+	int cantidadEnemigos;
+	int vidaEnemigos;
+	int defensaEnemigos;
+
+}
