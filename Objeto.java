dd0ea5dcc9f8dd9652191c96cf96bc7e3dd02641
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-16 18:20:10 -0400

    Update Objeto.java

--- Objeto.java
+++ Objeto.java
@@ -1,1 +1,28 @@
 
+packagage juego;
+
+public class Objeto{
+	String nombreObjeto;
+	String atributoObjeto;
+	int valorAtributo;
+
+	public Objeto(String nombre,String atributo,int valor){
+		nombreObjeto = nombre;
+		atributoObjeto = atributo;
+		valorAtributo = valor;
+
+	}
+	String getNombre(){
+		return nombreObjeto;
+
+	}
+
+	String getAtributo(){
+		return atributoObjeto;
+
+	}
+	int getValor{
+		return valorAtributo;
+
+	}
+}
