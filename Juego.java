1a222a396cc1285fce91ecd666064e828d0a8201
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-16 19:53:26 -0400

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -1,16 +1,37 @@
 
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Juego {
 
   public static void main(String[] args) {
   	Scanner scan = new Scanner(System.in);
   	//String s = scan.next();
-  	int i = scan.nextInt();
+  	//int i = scan.nextInt();
+  	try {
+		FileWriter writer = new FileWriter("MyFile.txt", true);
+		writer.write("Hello World");
+		writer.write("\r\n");   // write new line
+		writer.write("Good Bye!");
+		writer.close();
+	} catch (IOException e) {
+		e.printStackTrace();
+		}
 
-	System.out.println(i);
+	try {
+		FileReader reader = new FileReader("MyFile.txt");
+		int character;
 
-
+        while ((character = reader.read()) != -1) {
+        	System.out.print((char) character);
+        }
+        reader.close();
+		
+	} catch (IOException e) {
+		e.printStackTrace();
+    	}
 
    
   }
