10cf4dd95cbf82b98f1eb7d302314892de06629c
AlbireoImma <faam1612@gmail.com>
2017-11-16 18:48:47 -0300

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,7 +1,7 @@
 public class Jugador extends Personaje{
 	protected int ataque;
 	protected int defensa;
-	protected int punto_poder;
+    protected int punto_poder;
 	protected String nombre;
 	protected String esp_1;
 	protected int power_1;
@@ -10,12 +10,13 @@ public class Jugador extends Personaje{
 	protected int power_2;
 	protected int cost_2;
 
+
 	Jugador(int vid, int def, int ata, String name, int pp){
 		super(vid);
 		this.ataque = ata;
 		this.defensa = def;
-		this.nombre = name;
-		this.punto_poder = pp;
+        this.nombre = name;
+        this.punto_poder = pp;
 	}
 
 	int get_pp(){
@@ -32,5 +33,14 @@ public class Jugador extends Personaje{
 		this.esp_2 = name;
 		this.power_2 = power;
 		this.cost_2 = pp;
+	}
+
+	void display(){
+		System.out.println(nombre);
+		System.out.println("Vida: "+vida);
+		System.out.println("Ataque: "+ataque);
+		System.out.println("Defensa: "+defensa);
+		System.out.println("Ataque especial: "+esp_1+" "+power_1+" "+cost_1+"pp");
+		System.out.println("Ataque especial: "+esp_2+" "+power_2+" "+cost_2+"pp");
 	}
 }
