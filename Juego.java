4919371f170ae99261f1ae1c6799c81ff31fa4de
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-16 19:28:13 -0400

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -1,1 +1,17 @@
 
+import java.util.Scanner;
+
+public class Juego {
+
+  public static void main(String[] args) {
+  	Scanner scan = new Scanner(System.in);
+  	//String s = scan.next();
+  	int i = scan.nextInt();
+
+	System.out.println(i);
+
+
+
+   
+  }
+}
