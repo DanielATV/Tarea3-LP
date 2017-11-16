60f8261fe575899d7640c7fd6efb1c537e20b530
AlbireoImma <faam1612@gmail.com>
2017-11-16 18:08:47 -0300

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,7 +1,7 @@
 public class Jugador extends Personaje{
 	protected int ataque;
 	protected int defensa;
-    protected int punto_poder;
+	protected int punto_poder;
 	protected String nombre;
 
 	Jugador(int vid, int def, int ata, String name, int pp){
