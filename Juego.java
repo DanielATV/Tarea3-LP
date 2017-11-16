68b0abe1afe99e1594eadc361c4634cd2fa1393b
AlbireoImma <faam1612@gmail.com>
2017-11-16 18:48:20 -0300

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -1,1 +1,9 @@
-
+public class Juego {
+	    public static void main(String[] args) {
+	      Jugador A = new Jugador(100,20,35,"Ryu",23);
+	      A.set_esp1("Hadouken", 200 , 15);
+	      A.set_esp2("Shoryouken", 250 , 20);
+	      A.display();
+	      return;
+    }
+}
