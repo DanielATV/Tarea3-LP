10c9b2e78821b65c055eff395e3d9fcc453a05f8
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-17 21:57:33 -0400

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -4,34 +4,49 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+
 public class Juego {
 
   public static void main(String[] args) {
+  	String nombreNiv;
+  	String nombreJug;
+  	int vidaJug;
+  	int ataqJug;
+  	int defJug;
   	Scanner scan = new Scanner(System.in);
   	//String s = scan.next();
   	//int i = scan.nextInt();
+
   	try {
-		FileWriter writer = new FileWriter("MyFile.txt", true);
-		writer.write("Hello World");
-		writer.write("\r\n");   // write new line
-		writer.write("Good Bye!");
+		FileWriter writer = new FileWriter("niveles.txt", true);
+		System.out.println("Nombre del Nivel: ");
+		nombreniv = scan.next();
+		writer.write(nomreinv);
+		writer.write("\n");
+		System.out.println("Nombre del Jugador: ");
+		nombreJug = scan.next();
+		writer.write(nombreJug );
+		System.out.println("Vida del Jugador: ");
+		vidaJug = scan.nextInt()
+		writer.write(vidaJug );
+		System.out.println("Ataque del Jugador: ");
+		ataqJug = scan.nextInt()
+		writer.write(ataqJug );
+		System.out.println("Defensa del Jugador: ");
+		defJug = scan.nextInt()
+		writer.write(defJug );
+
+
+
+
+
+
+
 		writer.close();
 	} catch (IOException e) {
 		e.printStackTrace();
 		}
 
-	try {
-		FileReader reader = new FileReader("MyFile.txt");
-		int character;
-
-        while ((character = reader.read()) != -1) {
-        	System.out.print((char) character);
-        }
-        reader.close();
-		
-	} catch (IOException e) {
-		e.printStackTrace();
-    	}
 
    
   }
