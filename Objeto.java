5b258be8b0a09b800b4dba0d0caefd007faf7795
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-18 19:52:01 -0400

    Update Objeto.java

--- Objeto.java
+++ Objeto.java
@@ -1,27 +1,32 @@
-
 public class Objeto{
 	String nombreObjeto;
 	String atributoObjeto;
 	int valorAtributo;
 
-	public Objeto(String nombre,String atributo,int valor){
+	Objeto(){
+		nombreObjeto="";
+		atributoObjeto="";
+		valorAtributo=0;
+	}
+
+	Objeto(String nombre,String atributo,int valor){
 		nombreObjeto = nombre;
 		atributoObjeto = atributo;
 		valorAtributo = valor;
-
 	}
 	String getNombre(){
 		return nombreObjeto;
-
 	}
 
 	String getAtributo(){
 		return atributoObjeto;
-
 	}
+
 	int getValor(){
 		return valorAtributo;
+	}
 
+	void display(){
+		System.out.println(nombreObjeto+": "+valorAtributo+" "+atributoObjeto);
 	}
 }
-
