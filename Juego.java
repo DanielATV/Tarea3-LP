09bcb431aa7476ecd51b705609024b4885321d5e
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-18 11:13:34 -0400

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -19,89 +19,165 @@ public class Juego {
   	String desi;
   	String obj;
   	int cantEn;
+  	boolean juego = true;
   	Scanner scan = new Scanner(System.in);
   	//String s = scan.next();
   	//int i = scan.nextInt();
 
-  	System.out.println("Elegir ataques especiales:");
-	System.out.println("1. Crear Nivel");
-	System.out.println("2. Simular avance en Nivel");
-	System.out.println("3. Consultar informacion de Nivel");
-	System.out.println("4. Salir");
-	op = scan.nextInt();
-	if (op == 1){
+  	while(juego){
+  		
+  		System.out.println("Elegir ataques especiales:");
+		System.out.println("1. Crear Nivel");
+		System.out.println("2. Simular avance en Nivel");
+		System.out.println("3. Consultar informacion de Nivel");
+		System.out.println("4. Salir");
+		op = scan.nextInt();
+		if (op == 1){
 
-		try {
-			FileWriter writer = new FileWriter("niveles.txt", true);
-			System.out.println("Nombre del Nivel: ");
-			nombreNiv = scan.next();
-			writer.write(nombreNiv);
-			writer.write("\n");
-			System.out.println("Nombre del Jugador: ");
-			nombreJug = scan.next();
-			writer.write(nombreJug + " ");
-			System.out.println("Vida del Jugador: ");
-			vidaJug = scan.nextInt();
-			writer.write(vidaJug + " ");
-			System.out.println("Ataque del Jugador: ");
-			ataqJug = scan.nextInt();
-			writer.write(ataqJug + " ");
-			System.out.println("Defensa del Jugador: ");
-			defJug = scan.nextInt();
-			writer.write(defJug + " ");
-			System.out.println("Elegir ataques especiales:");
-			System.out.println("1. Guillotine Fist +200 ataque, 30pp");
-			System.out.println("2. Rising Dragon +500 ataque, 25pp");
-			System.out.println("3. Knuckle Arrow +100 ataque, 50pp");
-			System.out.println("4. Gate of Hell +5000 ataque, 5pp");
-			op = scan.nextInt();
-			op2 = scan.nextInt();
-			if (op == 1){
-				ataque1 = "Guillotine_Fist";
-				writer.write(ataque1 + " ");
+			try {
+				FileWriter writer = new FileWriter("niveles.txt", true);
+				System.out.println("Nombre del Nivel: ");
+				nombreNiv = scan.next();
+				writer.write(nombreNiv);
+				writer.write("\n");
+				System.out.println("Nombre del Jugador: ");
+				nombreJug = scan.next();
+				writer.write(nombreJug + " ");
+				System.out.println("Vida del Jugador: ");
+				vidaJug = scan.nextInt();
+				writer.write(vidaJug + " ");
+				System.out.println("Ataque del Jugador: ");
+				ataqJug = scan.nextInt();
+				writer.write(ataqJug + " ");
+				System.out.println("Defensa del Jugador: ");
+				defJug = scan.nextInt();
+				writer.write(defJug + " ");
+				System.out.println("Elegir ataques especiales:");
+				System.out.println("1. Guillotine Fist +200 ataque, 30pp");
+				System.out.println("2. Rising Dragon +500 ataque, 25pp");
+				System.out.println("3. Knuckle Arrow +100 ataque, 50pp");
+				System.out.println("4. Gate of Hell +5000 ataque, 5pp");
+				op = scan.nextInt();
+				op2 = scan.nextInt();
+				if (op == 1){
+					ataque1 = "Guillotine_Fist";
+					writer.write(ataque1 + " ");
 
-			}
-			else if (op == 2){
-				ataque1 = "Rising_Dragon";
-				writer.write(ataque1 + " ");
+				}
+				else if (op == 2){
+					ataque1 = "Rising_Dragon";
+					writer.write(ataque1 + " ");
 
-			}
-			else if (op == 3){
-				ataque1 = "Knuckle_ArrowFist";
-				writer.write(ataque1 + " ");
+				}
+				else if (op == 3){
+					ataque1 = "Knuckle_ArrowFist";
+					writer.write(ataque1 + " ");
 
-			}
-			else if (op == 4){
-				ataque1 = "Gate_of_Hell";
-				writer.write(ataque1 + " ");
+				}
+				else if (op == 4){
+					ataque1 = "Gate_of_Hell";
+					writer.write(ataque1 + " ");
 
-			}
-			if (op2 == 1){
-				ataque2 = "Guillotine_Fist";
-				writer.write(ataque2 + " ");
+				}
+				if (op2 == 1){
+					ataque2 = "Guillotine_Fist";
+					writer.write(ataque2 + " ");
 
-			}
-			else if (op2 == 2){
-				ataque2 = "Rising_Dragon";
-				writer.write(ataque2 + " ");
-
-
-			}
-			else if (op2 == 3){
-				ataque2 = "Knuckle_Arrow";
-				writer.write(ataque2 + " ");
+				}
+				else if (op2 == 2){
+					ataque2 = "Rising_Dragon";
+					writer.write(ataque2 + " ");
 
 
-			}
-			else if (op2 == 4){
-				ataque2 = "Gate_of_Hell";
-				writer.write(ataque2 + " ");
+				}
+				else if (op2 == 3){
+					ataque2 = "Knuckle_Arrow";
+					writer.write(ataque2 + " ");
 
-			}
 
-			System.out.println("Desea equipar un objeto? y/n");
-			desi = scan.next();
-			if( desi.equals("y")){
+				}
+				else if (op2 == 4){
+					ataque2 = "Gate_of_Hell";
+					writer.write(ataque2 + " ");
+
+				}
+
+				System.out.println("Desea equipar un objeto? y/n");
+				desi = scan.next();
+				if( desi.equals("y")){
+					System.out.println("1. Veil of Discord +600 defensa");
+					System.out.println("2. Chainmail +500 defensa");
+					System.out.println("3. Guardian Greaves + 500 vida");
+					System.out.println("4. Lotus Orb +1000 vida");
+					op = scan.nextInt();
+
+					if (op == 1){
+						obj = "Veil_of_Discord";
+						writer.write(obj);
+						writer.write("\n");
+
+					}
+					else if (op == 2){
+						obj = "Chainmail";
+						writer.write(obj);
+						writer.write("\n");
+
+						
+					}
+					else if (op == 3){
+						obj = "Guardian_Greaves";
+						writer.write(obj);
+						writer.write("\n");
+
+						
+					}
+					else if (op == 4){
+						obj = "Lotus_Orb";
+						writer.write(obj);
+						writer.write("\n");
+						
+					}
+
+
+				}
+				else if( desi.equals("n")){
+					writer.write("\n");
+
+				}
+
+				System.out.println("Nombre del Jefe: ");
+				nombreJug = scan.next();
+				writer.write(nombreJug + " ");
+				System.out.println("Vida del Jefe: ");
+				vidaJug = scan.nextInt();
+				writer.write(vidaJug + " ");
+				System.out.println("Ataque del Jefe: ");
+				ataqJug = scan.nextInt();
+				writer.write(ataqJug + " ");
+				System.out.println("Defensa del Jefe: ");
+				defJug = scan.nextInt();
+				writer.write(defJug + " ");
+				System.out.println("Elegir ataques especiales:");
+				System.out.println("1. Cannon Spear + 300 ataque");
+				System.out.println("2. Limit Break +10000 ataque");
+				op = scan.nextInt();
+				if (op == 1){
+					ataque1 = "Cannon_Spear";
+					writer.write(ataque1 + " ");
+					writer.write("\n");
+
+				}
+				else if (op == 2){
+					ataque1 = "Limit_Break ";
+					writer.write(ataque1 + " ");
+					writer.write("\n");
+
+				}
+
+				System.out.println("Vida del Aliado: ");
+				vidaJug = scan.nextInt();
+				writer.write(vidaJug + " ");
+				System.out.println("Objeto Aliado: ");
 				System.out.println("1. Veil of Discord +600 defensa");
 				System.out.println("2. Chainmail +500 defensa");
 				System.out.println("3. Guardian Greaves + 500 vida");
@@ -135,136 +211,73 @@ public class Juego {
 					
 				}
 
+				System.out.println("Objeto del Nivel ");
+				System.out.println("1. Veil of Discord +600 defensa");
+				System.out.println("2. Chainmail +500 defensa");
+				System.out.println("3. Guardian Greaves + 500 vida");
+				System.out.println("4. Lotus Orb +1000 vida");
+				op = scan.nextInt();
 
-			}
-			else if( desi.equals("n")){
+				if (op == 1){
+					obj = "Veil_of_Discord";
+					writer.write(obj);
+					writer.write("\n");
+
+				}
+				else if (op == 2){
+					obj = "Chainmail";
+					writer.write(obj);
+					writer.write("\n");
+
+					
+				}
+				else if (op == 3){
+					obj = "Guardian_Greaves";
+					writer.write(obj);
+					writer.write("\n");
+
+					
+				}
+				else if (op == 4){
+					obj = "Lotus_Orb";
+					writer.write(obj);
+					writer.write("\n");
+					
+				}
+
+				System.out.println("Cantidad de enemigos: ");
+				cantEn = scan.nextInt();
+				writer.write(vidaJug + " ");
+				System.out.println("Vida del enemigo: ");
+				vidaJug = scan.nextInt();
+				writer.write(vidaJug + " ");
+				System.out.println("Ataque del enemigo: ");
+				ataqJug = scan.nextInt();
+				writer.write(ataqJug + " ");
+				System.out.println("Defensa del enemigo: ");
+				defJug = scan.nextInt();
+				writer.write(defJug + " ");
 				writer.write("\n");
 
-			}
-
-			System.out.println("Nombre del Jefe: ");
-			nombreJug = scan.next();
-			writer.write(nombreJug + " ");
-			System.out.println("Vida del Jefe: ");
-			vidaJug = scan.nextInt();
-			writer.write(vidaJug + " ");
-			System.out.println("Ataque del Jefe: ");
-			ataqJug = scan.nextInt();
-			writer.write(ataqJug + " ");
-			System.out.println("Defensa del Jefe: ");
-			defJug = scan.nextInt();
-			writer.write(defJug + " ");
-			System.out.println("Elegir ataques especiales:");
-			System.out.println("1. Cannon Spear + 300 ataque");
-			System.out.println("2. Limit Break +10000 ataque");
-			op = scan.nextInt();
-			if (op == 1){
-				ataque1 = "Cannon_Spear";
-				writer.write(ataque1 + " ");
-				writer.write("\n");
-
-			}
-			else if (op == 2){
-				ataque1 = "Limit_Break ";
-				writer.write(ataque1 + " ");
-				writer.write("\n");
-
-			}
-
-			System.out.println("Vida del Aliado: ");
-			vidaJug = scan.nextInt();
-			writer.write(vidaJug + " ");
-			System.out.println("Objeto Aliado: ");
-			System.out.println("1. Veil of Discord +600 defensa");
-			System.out.println("2. Chainmail +500 defensa");
-			System.out.println("3. Guardian Greaves + 500 vida");
-			System.out.println("4. Lotus Orb +1000 vida");
-			op = scan.nextInt();
-
-			if (op == 1){
-				obj = "Veil_of_Discord";
-				writer.write(obj);
-				writer.write("\n");
-
-			}
-			else if (op == 2){
-				obj = "Chainmail";
-				writer.write(obj);
-				writer.write("\n");
 
 				
-			}
-			else if (op == 3){
-				obj = "Guardian_Greaves";
-				writer.write(obj);
-				writer.write("\n");
-
-				
-			}
-			else if (op == 4){
-				obj = "Lotus_Orb";
-				writer.write(obj);
-				writer.write("\n");
-				
-			}
-
-			System.out.println("Objeto del Nivel ");
-			System.out.println("1. Veil of Discord +600 defensa");
-			System.out.println("2. Chainmail +500 defensa");
-			System.out.println("3. Guardian Greaves + 500 vida");
-			System.out.println("4. Lotus Orb +1000 vida");
-			op = scan.nextInt();
-
-			if (op == 1){
-				obj = "Veil_of_Discord";
-				writer.write(obj);
-				writer.write("\n");
-
-			}
-			else if (op == 2){
-				obj = "Chainmail";
-				writer.write(obj);
-				writer.write("\n");
-
-				
-			}
-			else if (op == 3){
-				obj = "Guardian_Greaves";
-				writer.write(obj);
-				writer.write("\n");
-
-				
-			}
-			else if (op == 4){
-				obj = "Lotus_Orb";
-				writer.write(obj);
-				writer.write("\n");
-				
-			}
-
-			System.out.println("Cantidad de enemigos: ");
-			cantEn = scan.nextInt();
-			writer.write(vidaJug + " ");
-			System.out.println("Vida del enemigo: ");
-			vidaJug = scan.nextInt();
-			writer.write(vidaJug + " ");
-			System.out.println("Ataque del enemigo: ");
-			ataqJug = scan.nextInt();
-			writer.write(ataqJug + " ");
-			System.out.println("Defensa del enemigo: ");
-			defJug = scan.nextInt();
-			writer.write(defJug + " ");
-			writer.write("\n");
 
 
-			
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				}
+		}
+		else if (op == 2){
+			System.out.println("Test");
+		}
+		else if(op == 4){
+			juego = false;
+		}
+
+  	}
 
 
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			}
-	}
 
 
 	  	
