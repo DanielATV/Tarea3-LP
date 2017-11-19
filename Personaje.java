7202614a85176dceed911143687b37294d542dd4
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-19 12:05:11 -0300

    Update Personaje.java

--- Personaje.java
+++ Personaje.java
@@ -1,6 +1,21 @@
-package juego;
-  
-public abstract class Personaje{
-  int vida;
-  public abstract void crearPersonaje();
+abstract public class Personaje{
+	protected int vida;
+
+	Personaje(int vid){
+		vida = vid;
+	}
+
+	Personaje(){
+		vida=0;
+	}
+
+	public int get_vida(){
+		return vida;
+	}
+
+	public void set_vida(int vid){
+		vida = vid;
+	}
+
+	abstract int reduce_hp(int damage);
 }
