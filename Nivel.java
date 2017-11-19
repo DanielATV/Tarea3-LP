09a2b957a8870c350224692171b3ef23fc88587c
DanielATV <daniel.torov@sansano.usm.cl>
2017-11-19 17:11:40 -0400

    Update Nivel.java

--- Nivel.java
+++ Nivel.java
@@ -102,7 +102,7 @@ public class Nivel{
 			FileWriter writer = new FileWriter("registro_simulaciones.txt", true);
 			writer.write(n.get_name());
 			writer.write("\n");
-			writer.write("comienzoSimulaicon");
+			writer.write("comienzoSimulacion");
 			writer.write("\n");
 
 			System.out.println("Begin!");
@@ -110,14 +110,15 @@ public class Nivel{
 			while(flag != 0){
 				if (choice == 1) { 
 					// Objeto encontrado
-					System.out.println("Objeto encontrado");
-					writer.write("Jugador se encuentra con Objeto del nivel");
+					System.out.println(object.getNombre() + " encontrado");
+					writer.write(player.get_name()+" se encuentra con "+object.getNombre());
 					writer.write("\n");
 					player.set_obj(object,0);
+
 				} else if (choice == 2) {
 					// Pelea contra esbirro
 					System.out.println("Enfrentamiento con Enemigo");
-					writer.write("Jugador se encuentra con Enemigo");
+					writer.write(player.get_name()+" se encuentra con Enemigo");
 					writer.write("\n");
 
 
@@ -128,14 +129,15 @@ public class Nivel{
 							// Ataque normal hacia el esbirro
 
 							valor = player.atacar_enemigo(enemy) ;
-							System.out.println("Jugador ataca Enemigo, Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+"Enemigo: "+enemy.get_vida() );
-							writer.write("Jugador ataque Enemigo "+player.get_vida()+" "+ally.get_vida()+""+enemy.get_vida());
+							System.out.println(player.get_name()+" ataca Enemigo, "+player.get_name()+": "+player.get_vida()+" Aliado: "+ally.get_vida()+" Enemigo: "+enemy.get_vida() );
+							writer.write(player.get_name()+" ataque Enemigo "+player.get_vida()+" "+ally.get_vida()+" "+enemy.get_vida());
 							writer.write("\n");
 
 							if(valor == 0){
 								// Esbirro derrotado
 								System.out.println("Enemigo derrotado");
 								writer.write("Enemigo muere");
+								writer.write("\n");
 								break;
 							}
 						} else if (esp_or_not == 2) {
@@ -153,8 +155,8 @@ public class Nivel{
 
 							valor= player.ata_esp_enemigo(what_esp,enemy);
 
-							System.out.println("Jugador utilizo "+ ataq + "contra Enemigo, Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+"Enemigo: "+enemy.get_vida() );
-							writer.write("Jugador "+ataq+ "Enemigo "+player.get_vida()+" "+ally.get_vida()+""+enemy.get_vida());
+							System.out.println(player.get_name()+" utilizo "+ ataq + " contra Enemigo, Jugador: "+player.get_vida()+" Aliado: "+ally.get_vida()+" Enemigo: "+enemy.get_vida() );
+							writer.write("Jugador "+ataq+ " Enemigo "+player.get_vida()+" "+ally.get_vida()+" "+enemy.get_vida());
 							writer.write("\n");
 
 
@@ -163,6 +165,7 @@ public class Nivel{
 
 								System.out.println("Enemigo derrotado");
 								writer.write("Enemigo muere");
+								writer.write("\n");
 
 								break;
 							}
@@ -171,23 +174,31 @@ public class Nivel{
 						which_one = test.rand_atack(ally.get_vida());
 						if (which_one.equals("Aliado")) {
 							enemy.atacar_aliado(ally,player);
+
+							System.out.println("Enemigo ataca a Aliado, "+player.get_name()+": "+player.get_vida()+" Aliado: "+ally.get_vida()+" Enemigo: "+enemy.get_vida() );
+							writer.write("Enemigo ataca "+player.get_name()+" "+player.get_vida()+" "+ally.get_vida()+" "+enemy.get_vida());
+							writer.write("\n");
 							if (player.get_vida() == 0) {
 								// Casualidades de objeto el player muere
+
+								System.out.println("Game Over");
+								writer.write("Muerte de "+player.get_name());
+								writer.write("\n");
 								flag = 0;
 							}
 						} else if (which_one.equals("Jugador")) {
 
 							valor = enemy.atacar_jugador(player);
 
-							System.out.println("Enemigo ataca Jugador, Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+"Enemigo: "+enemy.get_vida() );
-							writer.write("Enemigo ataque Jugador "+player.get_vida()+" "+ally.get_vida()+""+enemy.get_vida());
+							System.out.println("Enemigo ataca a"+player.get_name()+", "+player.get_name()+": "+player.get_vida()+" Aliado: "+ally.get_vida()+" Enemigo: "+enemy.get_vida() );
+							writer.write("Enemigo ataca "+player.get_name()+" "+player.get_vida()+" "+ally.get_vida()+" "+enemy.get_vida());
 							writer.write("\n");
 
 							if( valor == 0){
 								// El esbirro mata al player
 
 								System.out.println("Game Over");
-								writer.write("Muerte del Jugador");
+								writer.write("Muerte de "+player.get_name());
 								writer.write("\n");
 								flag = 0;
 							}
@@ -201,6 +212,10 @@ public class Nivel{
 					System.out.println("WARNING!!");
 					System.out.println("Batalla final");
 
+					writer.write(player.get_name()+" se encuentra con "+boss.get_nombre());
+					writer.write("\n");
+
+
 					while(boss.get_vida()>0 && player.get_vida()>0){
 						// Ataque del player
 
@@ -211,16 +226,16 @@ public class Nivel{
 
 							valor = player.atacar_jefe(boss);
 
-							System.out.println("Jugador ataca "+boss.get_nombre()+", Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
-							writer.write("Jugador ataca "+boss.get_nombre()+", Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
+							System.out.println(player.get_name()+" ataca a "+boss.get_nombre()+", "+player.get_name()+": "+player.get_vida()+" Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_nombre()+" "+boss.get_vida() );
+							writer.write(player.get_name()+" ataca "+boss.get_nombre()+" "+player.get_vida()+" "+ally.get_vida()+" "+boss.get_vida() );
 							writer.write("\n");
 
 
 							if(player.atacar_jefe(boss) == 0){
 								// jefe derrotado
-
+								System.out.println(boss.get_nombre()+ "ha sido derrotado");
 								System.out.println("Salvaste al mundo!");
-								writer.write("Muerte del Jefe");
+								writer.write("Muerte de "+boss.get_nombre());
 								writer.write("\n");
 
 								flag = 0;
@@ -238,15 +253,16 @@ public class Nivel{
 
 							}
 							 valor = player.ata_esp_jefe(what_esp,boss);
-							System.out.println("Jugador utilizo "+ ataq +"contra "+boss.get_nombre()+", Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
-							writer.write("Jugador "+ataq+ "Jefe"+player.get_vida()+" "+ally.get_vida()+""+boss.get_vida());
+							System.out.println(player.get_name()" utilizo "+ ataq +"contra "+boss.get_nombre()+", "player.get_name()": "+player.get_vida()+"Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
+							writer.write(player.get_name()" "+ataq+" "+boss.get_nombre()+" "+player.get_vida()+" "+ally.get_vida()+" "+boss.get_vida());
 							writer.write("\n");
 
 							if(valor == 0){
 								// jefe derrotado
 
+								System.out.println(boss.get_nombre()+ "ha sido derrotado");
 								System.out.println("Salvaste al mundo!");
-								writer.write("Muerte del Jefe");
+								writer.write("Muerte de "+boss.get_nombre());
 								writer.write("\n");
 								flag = 0;
 							}
@@ -273,21 +289,32 @@ public class Nivel{
 
 								valor = boss.atacar_jugador(player);
 
-								System.out.println(boss.get_nombre()+"ataca Jugador, Jugador:"+player.get_vida()+"Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
-								writer.write("Jefe ataque Jugador "+player.get_vida()+" "+ally.get_vida()+""+boss.get_vida());
+								System.out.println(boss.get_nombre()+"ataca "player.get_name()", "player.get_name()": "+player.get_vida()+" Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
+								writer.write(boss.get_nombre()" ataque "player.get_name()" "+player.get_vida()+" "+ally.get_vida()+" "+boss.get_vida());
 								writer.write("\n");
+
 								if(valor == 0){
 									// El jefe mata al player
 
 									System.out.println("Game Over");
-									writer.write("Muerte del Jugador");
+									writer.write("Muerte de "+player.get_name());
 									writer.write("\n");
 
 									flag = 0;
 								}
 							} else if (esp_or_not == 2) {
-								if (boss.ata_esp_jugador(player) == 0) {
+
+								valor = boss.ata_esp_jugador(player)
+
+								System.out.println(boss.get_nombre()+" utilizo "+boss.get_esp()+" contra "+player.get_name()", "player.get_name()": "+player.get_vida()+" Aliado: "+ally.get_vida()+" "+boss.get_nombre()+" "+boss.get_vida() );
+								writer.write(boss.get_nombre()" "+boss.get_esp()+" "player.get_name()" "+player.get_vida()+" "+ally.get_vida()+" "+boss.get_vida());
+								writer.write("\n");
+
+								if ( valor == 0) {
 									// El jefe mata al player
+									System.out.println("Game Over");
+									writer.write("Muerte de "+player.get_name());
+									writer.write("\n");
 									flag = 0;
 								}
 							}
@@ -302,7 +329,7 @@ public class Nivel{
 			System.out.println("Fin de la simulacion");
 			writer.write("finalSimulacion");
 			writer.write("\n");
-			writer.write(player.get_vida()+" "+ally.get_vida()+""+boss.get_vida()+" "+ cant);
+			writer.write(player.get_vida()+" "+ally.get_vida()+" "+boss.get_vida()+" "+cant);
 			writer.write("\n");
 			writer.close();
 			
