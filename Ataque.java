010a0f61c2747c2bff76f04d3e993fc763ab6baf
AlbireoImma <faam1612@gmail.com>
2017-11-19 23:15:11 -0300

    Update Ataque.java

--- Ataque.java
+++ Ataque.java
@@ -1,3 +1,14 @@
 public interface Ataque{
+	
+	/******** Metodo: atacar *********************************
+		Descripcion: Calcula el daño a recibir mediante 
+			     un ataque normal
+		Parametros:
+			def entero
+			int vid
+		Retorno: Retorna la cantidad de vida a 
+			 perder derivado del ataque
+	*********************************************************/
+	
 	public int atacar(int def, int vid);
 }
