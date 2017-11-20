2428a581fca8aee2ed9cf598cbe6b7d04fe54054
AlbireoImma <faam1612@gmail.com>
2017-11-19 23:16:06 -0300

    Update AtaqueEspecial.java

--- AtaqueEspecial.java
+++ AtaqueEspecial.java
@@ -1,4 +1,16 @@
 public interface AtaqueEspecial{
+	
+	/******** Metodo: ataque_especial ************************
+		Descripcion: Calcula el daño a recibir mediante 
+			     un ataque especial
+		Parametros:
+			opc entero
+			def entero
+			int vid
+		Retorno: Retorna la cantidad de vida a 
+			 perder derivado del ataque especial
+	*********************************************************/
+	
 	public int ataque_especial(int opc,int vida, int def);
 }
 
